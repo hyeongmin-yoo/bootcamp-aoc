fb635f4b34a708c40d45bffae48a514e9f60578e
hyeongmin.yoo <hyeongmin.yoo@greenlabs.co.kr>
2022-11-19 11:10:15 +0900

    Result 함수들 추가

--- rescript/lib/js/src/Util.js
+++ rescript/lib/js/src/Util.js
@@ -3,6 +3,8 @@
 
 var Curry = require("rescript/lib/js/curry.js");
 var Belt_Array = require("rescript/lib/js/belt_Array.js");
+var Belt_Result = require("rescript/lib/js/belt_Result.js");
+var Caml_option = require("rescript/lib/js/caml_option.js");
 
 function tap(arr, fn) {
   return Belt_Array.map(arr, (function (it) {
@@ -27,11 +29,56 @@ function toArray$1(str) {
   return Array.from(str);
 }
 
+function divide(str, anchor) {
+  var first = str.slice(0, anchor);
+  var last = str.slice(anchor);
+  return [
+          first,
+          last
+        ];
+}
+
 var $$String = {
-  toArray: toArray$1
+  toArray: toArray$1,
+  divide: divide
+};
+
+function toOption(result) {
+  if (result.TAG === /* Ok */0) {
+    return Caml_option.some(result._0);
+  }
+  
+}
+
+function traverse(results) {
+  return Belt_Array.reduce(results, {
+              TAG: /* Ok */0,
+              _0: []
+            }, (function (r, it) {
+                return Belt_Result.flatMap(r, (function (prev) {
+                              return Belt_Result.map(it, (function (val) {
+                                            return Belt_Array.concat(prev, [val]);
+                                          }));
+                            }));
+              }));
+}
+
+function fold(result, err, ok) {
+  if (result.TAG === /* Ok */0) {
+    return Curry._1(ok, result._0);
+  } else {
+    return Curry._1(err, result._0);
+  }
+}
+
+var Result = {
+  toOption: toOption,
+  traverse: traverse,
+  fold: fold
 };
 
 exports.$$Array = $$Array;
 exports.Input = Input;
 exports.$$String = $$String;
+exports.Result = Result;
 /* No side effect */
--- rescript/src/Util.res
+++ rescript/src/Util.res
@@ -18,4 +18,30 @@ module String = {
     open Js
     str->String2.castToArrayLike->Array2.from
   }
+
+  let divide = (str, anchor): (string, string) => {
+    let first = str->Js.String2.slice(~from=0, ~to_=anchor)
+    let last = str->Js.String2.sliceToEnd(~from=anchor)
+    (first, last)
+  }
+}
+
+module Result = {
+  let toOption = (result: result<'a, 'b>) => {
+    switch result {
+    | Ok(val) => Some(val)
+    | Error(_) => None
+    }
+  }
+  let traverse = (results: array<result<'a, 'b>>): result<array<'a>, 'b> => {
+    results->Belt.Array.reduce(Ok([]), (r, it) =>
+      r->Belt.Result.flatMap(prev => it->Belt.Result.map(val => prev->Belt.Array.concat([val])))
+    )
+  }
+  let fold = (result: result<'a, 'b>, err: 'b => unit, ok: 'a => unit): unit => {
+    switch result {
+    | Ok(val) => ok(val)
+    | Error(val) => err(val)
+    }
+  }
 }
